743f73198e9c162d040213a664ae61926ce1a996
Fatimaezzahralah <fatimaezzahra288@gmail.com>
2023-10-29 16:16:05 -0700

    tasks

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,7 +1,13 @@
 #!/usr/bin/node
-// Sort a dictionary of occurrences by user ids by occurrence
-const dict = require('./100-data').dict;
-const newDict = {};
-for (const val of Object.values(dict)) {
-	console.log(`key -> ${val}`);
+const dict = require('./101-data').dict;
+const dKeys = Object.keys(dict);
+const values = Object.values(dict);
+let matched;
+const result = {};
+// loop over the values
+for (let i = 0; i < values.length; i++) {
+  result[JSON.stringify(values[i])] = [];
+  matched = dKeys.filter(key => dict[key] === values[i]);
+  matched.forEach(item => result[JSON.stringify(values[i])].push(item));
 }
+console.log(result);
